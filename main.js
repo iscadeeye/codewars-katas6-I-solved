c207a645af97fb435ddf4017373c2a386e5aeb61
mohamed hassan <hdeeq414@gmail.com>
2021-09-14 22:09:36 -0500

    Highest Scoring Word kata is solved

--- main.js
+++ main.js
@@ -103,3 +103,86 @@ function likes (names) {
 
 console.assert(likes(['John', 'Mohamed']) === 'John and Mohamed like this')
 console.assert(likes([]) === 'no one likes this')
+
+//highest scoring word
+
+function stringFunction (word) {
+  let ObOfLetters = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+    g: 7,
+    h: 8,
+    i: 9,
+    j: 10,
+    k: 11,
+    l: 12,
+    m: 13,
+    n: 14,
+    o: 15,
+    p: 16,
+    q: 17,
+    r: 18,
+    s: 19,
+    t: 20,
+    u: 21,
+    v: 22,
+    w: 23,
+    x: 24,
+    y: 25,
+    z: 26
+  }
+  let letters = word.split('')
+  let counter = 0
+  for (let i = 0; i < letters.length; i += 1) {
+    currentLetter = letters[i]
+    counter += ObOfLetters[currentLetter]
+  }
+  return counter
+}
+
+function high (x) {
+  //turn the string into array of words first, and loop throgh the array.
+  let result
+  let hightestScore = 0
+  let hightestScoringWord = ''
+  let arrayOfWords = x.split(' ')
+  if (arrayOfWords.length === 1) {
+    hightestScoringWord = arrayOfWords[0]
+    return hightestScoringWord // exit the function early.
+  }
+  for (let i = 0; i < arrayOfWords.length; i += 1) {
+    let currentWord = arrayOfWords[i]
+     //then each iteration, call this function to calculate the word's score.
+    result = stringFunction(currentWord)
+    if (hightestScore < result) {
+      hightestScore = result
+      hightestScoringWord = arrayOfWords[i]
+    }
+  }
+  return hightestScoringWord
+}
+
+
+//unit tests
+console.assert(high('man i need a taxi up to ubud') === 'taxi', {
+  expected: 'taxi',
+  result: high('man i need a taxi up to ubud')
+})
+
+console.assert(high('what time are we climbing up the volcano') === 'volcano', {
+  expected: 'volcano',
+  result: high('what time are we climbing up the volcano')
+})
+
+console.assert(high('take me to semynak') === 'semynak', {
+  expected: 'semynak',
+  result: high('take me to semynak')
+})
+console.assert(high('take') === 'take', {
+  expected: 'take',
+  result: high('take')
+})
